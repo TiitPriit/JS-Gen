9f62ed7c5bdfc5f6ba51c4b995cef0f0ec68b902
Oskar.Kallas <112690706+TiitPriit@users.noreply.github.com>
2023-04-11 11:37:31 +0300

    basik js

---
+++ app.js
@@ -0,0 +1,15 @@
+// Variables
+
+const myName = "Oskar Kallas"; // String
+let myAge = 16; // Number
+const myChildren = ["Gen-junior", "Karl-junior"]
+const canRideBycicle = false;
+const person = {
+    myName: "Oskar Kallas",
+    myAge: 16,
+};
+
+console.log(myName, myAge);
+console.log(myChildren);
+console.log(canRideBycicle);
+console.log(person)
