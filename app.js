b9c61c0ee83ddc4a0c145f487c3c74f85a556518
Oskar.Kallas <112690706+TiitPriit@users.noreply.github.com>
2023-04-18 11:42:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,3 +13,47 @@ console.log(myName, myAge);
 console.log(myChildren);
 console.log(canRideBycicle);
 console.log(person)
+
+//
+const contacts = new Map();
+
+contacts.set("Jessie", { phone: "55555555", aadress: "ametikool"});
+contacts.has("Jessie");
+contacts.get("Jessie");
+contacts.set("Audi", { model: "S8"});
+console.log(contacts.size)
+contacts.delete("Audi");
+
+const mySet = new Set();
+
+mySet.add("1");
+mySet.add({a: 1, b: 2});
+mySet.add(true);
+
+// JSON - JavaScript Object Notation
+
+const thisIsJSON ={
+    "name" : "Kallas Oskar",
+    "age" : 191,
+}
+
+// if statements and shorthand expressions
+
+let year = prompt("Kui vana sa oled?");
+
+//Long version
+if (year === "2006") {
+    alert("õige vastus, vanamees. ")
+} else if (year === "2005"){
+    alert("Natuke rohkem.")
+} else {
+    alert("puhta mõõda.")
+}
+
+//shorthand version
+
+year === "1756" 
+? alert("Pihtas põhjas") 
+: alert ("mõõda")
+
+
