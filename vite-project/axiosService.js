0a66e55d0a76ff976353cfe03926892332c34320
Oskar.Kallas <112690706+TiitPriit@users.noreply.github.com>
2023-09-27 09:22:17 +0300

    MAIN

--- vite-project/axiosService.js
+++
@@ -1,9 +0,0 @@
-
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  baseURL: "http://creativecommons.org/licenses/by-nc-sa/3.0/",
-  timeout: 5000,
-});
-
-export default axiosInstance;
--- vite-project/main.js
+++ vite-project/main.js
@@ -1,24 +1,31 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+import axios from "axios";
+
+const url = "https://dog.ceo/api/breeds/image/random";
+
+const getDog = () => {
+    axios.get(url)
+        .then(response => {
+            let image = response.data.message;
+            document.getElementById("image").src = image;
+        })
+        .catch(error => {
+            console.error("Viga päringu tegemisel:", error);
+        });
+}
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+<h1>Gen pani mulle MA seda isegi ei teinud, ma ei tea, miks ma pean sellele tähelepanu pöörama, aga siin on koerapilt:</h1>
 
-setupCounter(document.querySelector('#counter'))
+<h2>Siin on mingi suvaline krants</h2>
+
+<button onclick="getDog()">Click-me</button>
+<br>
+<br>
+
+<img id="image" src="" alt="Dog" width="350px" height="350px" />
+`;
+
+
+window.getDog = getDog;
+
